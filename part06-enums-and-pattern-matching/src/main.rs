ffdf28b1ce56aaaa555cf9cb12997eb67df8e1e0
Daniel P. Doyle <3020488+TheDanielDoyle@users.noreply.github.com>
2024-10-03 21:59:38 +0100

    WIP: Working out modules within enum section
    Co-Authored-By: Alex Hedley <1573469+AlexHedley@users.noreply.github.com>
    Co-Authored-By: Adam Parker <30392071+grab-a-byte@users.noreply.github.com>

--- part06-enums-and-pattern-matching/src/main.rs
+++ part06-enums-and-pattern-matching/src/main.rs
@@ -1,5 +1,12 @@
+use message::Message::*;
+
 // 6. Enums and Pattern Matching
 fn main() {
+
+    // my_mod::function();
+    // message::Message
+    // message::Message::*;
+
     // 6.1 Defining an Enum
     #[derive(Debug)]
     enum IpAddrKind {
@@ -38,8 +45,131 @@ fn main() {
     }
 
     let home1 = IpAddr::V6(String::from("127.0.0.1"));
+    // let loopback = IpAddr::V6(String::from("::1"));
     let home2 = IpAddr::V4(127, 0, 0, 1);
     if let IpAddr::V4(p1, p2, p3, p4) = home2 {
        println!("{}, {}, {}, {}", p1, p2, p3 ,p4)
     }
+
+    // struct Ipv4Addr {
+    //     // --snip--
+    // }
+
+    // struct Ipv6Addr {
+    //     // --snip--
+    // }
+
+    // enum IpAddr {
+    //     V4(Ipv4Addr),
+    //     V6(Ipv6Addr),
+    // }
+
+    // enum Message {
+    //     Quit,
+    //     Move { x: i32, y: i32 },
+    //     Write(String),
+    //     ChangeColor(i32, i32, i32),
+    // }
+
+    struct QuitMessage; // unit struct
+    struct MoveMessage {
+        x: i32,
+        y: i32,
+    }
+    struct WriteMessage(String); // tuple struct
+    struct ChangeColorMessage(i32, i32, i32); // tuple struct
+
+    // impl Message {
+    //     fn call(&self) {
+    //         // method body would be defined here
+    //     }
+    // }
+    //
+    // let m = Message::Write(String::from("hello"));
+    // m.call();
+
+    // enum Option<T> {
+    //     None,
+    //     Some(T),
+    // }
+
+    let some_number = Some(4);
+    let some_char = Some('a');
+
+    let absent_number: Option<i32> = None; // core::option::Option::None
+
+    // let x: i8 = 5;
+    // let y: Option<i8> = Some(5);
+    //
+    // let sum = x + y;
+
+    enum Coin {
+        Penny,
+        Nickel,
+        Dime,
+        Quarter,
+    }
+
+    let c = Coin::Penny;
+    let amount = match c {
+        Coin::Penny => 1,
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter => 25,
+    };
+
+    fn value_in_cents(coin: Coin) -> u8 {
+        match coin {
+            Coin::Penny => {
+                println!("Lucky penny!");
+                1
+            }
+            Coin::Nickel => 5,
+            Coin::Dime => 10,
+            Coin::Quarter => 25,
+        }
+    }
+    let _ = value_in_cents(Coin::Nickel);
+
+    #[derive(Debug)] // so we can inspect the state in a minute
+    enum UsState {
+        Alabama,
+        Alaska,
+        // --snip--
+    }
+    let state = UsState::Alabama;
+
+
+    fn plus_one(x: Option<i32>) -> Option<i32> {
+        match x {
+            None => None,
+            Some(i) => Some(i + 1),
+        }
+    }
+
+    let five = Some(5);
+    let six = plus_one(five);
+    let none = plus_one(None);
+
+    // enum Message {
+    //     Quit,
+    //     Move { x: i32, y: i32 },
+    //     Write(String),
+    //     ChangeColor(i32, i32, i32),
+    // }
+
+    let message = ChangeColor(1,2,3);
+
+    // use Message::Quit;
+
+    let y = match message {
+        Quit => {},
+        Move { x, y } => {
+            println!("{}", x);
+            println!("{}", y);
+        },
+        Write(_) => {},
+        ChangeColor(_, _, _) => {},
+    };
+
 }
